f050ef1f5c0f70568e3ce0365940f1e9f1c51d6b
jagym105 <37801624+jagym105@users.noreply.github.com>
2022-12-09 14:05:49 +0900

    Update main.py

--- code/main.py
+++ code/main.py
@@ -2,7 +2,9 @@
 import subprocess
 import re
 import json
-from anytree import Node, RenderTree, exporter
+from anytree import Node, RenderTree,exporter
+import csv
+import os
 
 
 #daemon까지가 들어가서 실행
@@ -10,8 +12,13 @@ from anytree import Node, RenderTree, exporter
 
 # 함수
 def nodecmd(command, option):
+    # node = Path + "-regtest -rpcport=1234 -datadir="+DataPath
     node = Path + "-regtest -rpcport=1234 -datadir="+DataPath
-    output = subprocess.getoutput(node +" "+ command +" "+ option)
+    try:
+        output = subprocess.getoutput(node +" "+ command +" "+ option)
+    except:    # 예외가 발생했을 때 실행됨
+        output = 'error'
+    
     return output
 
 def getblockchaininfo():
@@ -47,17 +54,19 @@ def savetx(txlist, blocknumber):
         #hex값 해독
         txinfo = str(nodecmd("decoderawtransaction", getrawtx))
         #vin 개수
-        count_vin = txinfo.count("vin")
+        if txinfo != 'error' :
+            
+            count_vin = txinfo.count("vin")
 
-        #vin,vout 파싱
-        vinmatch = re.compile('\[[^]]*\]')
-        vin = vinmatch.findall(txinfo)
+            #vin,vout 파싱
+            vinmatch = re.compile('\[[^]]*\]')
+            vin = vinmatch.findall(txinfo)
 
-        #tx pointer값 가져오기
-        pointermatch = re.compile("[a-z0-9]{63,64}")
-        pointer = pointermatch.findall(str(vin))
+            #tx pointer값 가져오기
+            pointermatch = re.compile("[a-z0-9]{63,64}")
+            pointer = pointermatch.findall(str(vin))
 
-        data[i] = {'Info' : txlist[i], 'Pointer' : pointer[0:count_vin]}
+            data[i] = {'Info' : txlist[i], 'Pointer' : pointer[0:count_vin]}
 
 
 
@@ -67,6 +76,7 @@ def savetx(txlist, blocknumber):
 
 
 def searchnexttx(searchtxinfo, blocknumber,tree):
+
     i = 0
     nexttxlist = []
     for i in range (1, blocknumber+1):
@@ -80,8 +90,18 @@ def searchnexttx(searchtxinfo, blocknumber,tree):
                     nexttxlist.append(json_data[str(j)]['Info'])
                     #json파일을 불러온 후, pointer값이 searchtxinfo와 같으면, info값을 list에 추가
     for i in nexttxlist:
-        child_Node = Node('tx',data=i,parent=tree)
-        searchnexttx(i,blocknumber,child_Node)
+        child_Node = Node(i,data=tx_to_walletAddress(i),parent=tree)
+
+        for n in range(len(addressData)):
+            if (child_Node.data[0] == addressData[n][0]) or (child_Node.data[1] == addressData[n][0]) :
+                child_Node.name = addressData[n][1]
+                break
+            else:
+                if n == len(addressData)-1:
+                    searchnexttx(i,blocknumber,child_Node)
+        
+        
+        
 
 def tx_to_walletAddress(tx_address):
     raw_tx_result = nodecmd("getrawtransaction", tx_address)
@@ -112,24 +132,40 @@ def main():
     blocknumber = getblockchaininfo()
     searchtx = str(input("searchtx: "))
 
-    for i in range(1, blocknumber+1, 1):
-        txlist = loadblock(str(i))
-        #savetx(txlist, str(i))
+    #638692 ~ 643484
+    #for i in range(638690, blocknumber+1, 1):
+    for i in range(1, blocknumber+1,1):
+        if not os.path.isfile(TxdbPath+str(i)+".json") :
+            txlist = loadblock(str(i))
+            savetx(txlist, str(i))
+
+
+    f = open('WalletAddress.csv','r',encoding='utf-8')
+    global addressData
+    addressData = list()
+    rea = csv.reader(f)
+    for row in rea:
+        addressData.append(row)
+    f.close
+    #walletAddress -> list 형태로 변환
 
     root = Node('root',data=searchtx)
     searchnexttx(searchtx, blocknumber,root)
-    copy_tree = root
+    # copy_tree = root
     for row in RenderTree(root):
         pre, fill, node = row
         print(f"{pre}{node.name}, data: {node.data}")
     print("\n\n")
-    for row in RenderTree(copy_tree):
-        pre, fill, node = row
-        node.data = tx_to_walletAddress(node.data)
-        print(f"{pre}{node.name}, data: {node.data}")
-    exporter.DotExporter(root).to_picture("C:\\Users\\JaeKyeom\\Desktop\\Bitcoin_Asset_Tracking\\test.dot")
+    # for row in RenderTree(copy_tree):
+    #     pre, fill, node = row
+    #     node.name = tx_to_walletAddress(node.name)
+    #     # node.data = tx_to_walletAddress(node.data)
+    #     print(f"{pre}{node.name}, data: {node.data}")
+    exporter.DotExporter(root).to_picture("test.png")
+
     
 
 if __name__ == '__main__':
     main()
 #181420f3e6f6207058db8ea9f7379bc6c0d4552acf2db8f4ce57968bb449b33f
+#graphviz lib 설치 필요 pip로 하면 안되고 https://graphviz.org/download/
